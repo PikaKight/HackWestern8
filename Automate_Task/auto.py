233d662a18feec11851bb0601985d6ef5fe15146
PikaKight <tmmarcus73@gmail.com>
2021-11-21 01:41:29 -0500

    got it working for logging, must use python terminal to enter username and password, this will be changed later

--- Automate_Task/auto.py
+++ Automate_Task/auto.py
@@ -12,8 +12,15 @@ def login(driver):
     driver.find_element(By.ID, "loginLink").click()
     time.sleep(1)
     driver.find_element(By.XPATH, "//button[text()[contains(.,'Western Student Login')]]").click()
-    
+    time.sleep(1)
 
+    username = driver.find_element(By.ID, "userId")
+    password = driver.find_element(By.ID, "password")
+
+    username.send_keys(input())
+    password.send_keys(input())
+
+    driver.find_element(By.NAME, "submit").click()
     
 def auto():
     PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -29,7 +36,7 @@ def auto():
             EC.element_to_be_clickable((By.XPATH, "div[@onclick=window.location='/Program/GetProgramDetails?courseId=4cbf8cd1-3fca-4e35-b331-a8dbb9b5a86f&semesterId=81dac0e7-2456-44c9-bfe4-6ed494cc6824']")).click()
         )
     except:
-        time.sleep(30)
+        time.sleep(5)
         
         driver.quit()
 
